996bba123bf1effb0bb4d480542c6c364b09b058
mvuco00 <mvuco00@fesb.hr>
2021-04-10 23:08:38 +0200

    adding sunset and sunrise time

--- weatherapp/src/components/Home/Home.js
+++ weatherapp/src/components/Home/Home.js
@@ -3,6 +3,17 @@ import axios from "axios";
 import CurrentWeather from "../CurrentWeather/CurrentWeather";
 import FiveDaysWeather from "../FiveDaysWeather/FiveDaysWeather";
 import Search from "../Search/Search";
+import { GiSunrise, GiSunset } from "react-icons/gi";
+
+const timestamp = (time) => {
+  let unix_timestamp = time;
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(unix_timestamp * 1000);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var seconds = "0" + date.getSeconds();
+  return hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2) + " h";
+};
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -41,6 +52,22 @@ const Home = () => {
         input={input}
       />
       {Object.keys(data).length !== 0 ? <CurrentWeather data={data} /> : null}
+      {Object.keys(data).length !== 0 && (
+        <div className="sun">
+          <div className="sunrise">
+            <div className="sun-icon">
+              <GiSunrise />
+            </div>
+            <div>{timestamp(data.data.sys.sunrise)}</div>
+          </div>
+          <div className="sunset">
+            <div className="sun-icon">
+              <GiSunset />
+            </div>
+            <div>{timestamp(data.data.sys.sunset)}</div>
+          </div>
+        </div>
+      )}
       {Object.keys(data).length !== 0 ? <FiveDaysWeather city={city} /> : null}
     </div>
   );
