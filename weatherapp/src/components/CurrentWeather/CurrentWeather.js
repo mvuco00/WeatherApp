58fced778978d86da7df75e632b12057809577a4
mvuco00 <mvuco00@fesb.hr>
2021-04-14 19:22:00 +0200

    changing icon based on weather description

--- weatherapp/src/components/CurrentWeather/CurrentWeather.js
+++ weatherapp/src/components/CurrentWeather/CurrentWeather.js
@@ -2,17 +2,38 @@ import React from "react";
 import { WiHumidity } from "react-icons/wi";
 import { CgCompressV } from "react-icons/cg";
 import { FiWind } from "react-icons/fi";
-import { RiTempColdLine } from "react-icons/ri";
+import { RiTempColdLine, RiMistFill } from "react-icons/ri";
+import {
+  TiWeatherCloudy,
+  TiWeatherDownpour,
+  TiWeatherStormy,
+  TiWeatherSunny,
+  TiWeatherSnow,
+} from "react-icons/ti";
 
 const CurrentWeather = ({ data }) => {
+  const description = data.data.weather[0].description;
   return (
     <div className="weather">
       <div className="weather-info">
         <div className="weather-icon-text">
-          <img
-            src={`http://openweathermap.org/img/w/${data.data.weather[0].icon}.png`}
-            alt={data.data.weather[0].description}
-          />
+          <div className="weather-icon">
+            {description.includes("clouds") ? (
+              <TiWeatherCloudy />
+            ) : description.includes("rain") ? (
+              <TiWeatherDownpour />
+            ) : description.includes("thunderstorm") ? (
+              <TiWeatherStormy />
+            ) : description.includes("snow") ? (
+              <TiWeatherSnow />
+            ) : description.includes("clear") ? (
+              <TiWeatherSunny />
+            ) : description.includes("mist") ? (
+              <RiMistFill />
+            ) : (
+              <TiWeatherCloudy />
+            )}
+          </div>
           <h4 className="weather-description">
             {data.data.weather[0].description}
           </h4>
